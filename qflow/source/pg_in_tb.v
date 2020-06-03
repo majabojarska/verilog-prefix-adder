51f8e67b48810034d986ef823f086aedff015570
Maja Bojarska <majabojarska98@gmail.com>
2020-06-03 18:25:05 +0200

    Separate testbenches from their respective modules

---
+++ qflow/source/pg_in_tb.v
@@ -0,0 +1,33 @@
+`include "pg_in.v"
+
+// Testbench
+module pg_in_tb;
+
+reg x, y, c_in;
+wire gen, prop;
+
+initial begin 
+    $monitor ("inputs: x=%b, y=%b, c_in=%b | outputs: gen=%b, prop=%b", x, y, c_in, gen, prop);
+
+    {x, y, c_in} = 3'b000;
+    #1 {x, y, c_in} = 3'b010;
+    #1 {x, y, c_in} = 3'b100;
+    #1 {x, y, c_in} = 3'b110;
+    #1 {x, y, c_in} = 3'b001;
+    #1 {x, y, c_in} = 3'b011;
+    #1 {x, y, c_in} = 3'b101;
+    #1 {x, y, c_in} = 3'b111;
+
+    #1 $finish;
+end
+
+// UUT
+pg_in U0 (
+    .x (x),
+    .y (y),
+    .c_in (c_in),
+    .gen (gen),
+    .prop (prop)
+);
+
+endmodule
---
+++ qflow/source/pg_tb.v
@@ -0,0 +1,28 @@
+`include "pg.v"
+
+// Testbench
+module pg_tb;
+
+reg x, y;
+wire gen, prop;
+
+initial begin 
+    $monitor ("inputs: x=%b, y=%b | outputs: gen=%b, prop=%b", x, y, gen, prop);
+    
+    {x, y} = 2'b00;
+    #1 {x, y} = 2'b01;
+    #1 {x, y} = 2'b10;
+    #1 {x, y} = 2'b11;
+    
+    #1 $finish;
+end
+
+// UUT
+pg U0 (
+    .x (x),
+    .y (y),
+    .gen (gen),
+    .prop (prop)
+);
+
+endmodule
