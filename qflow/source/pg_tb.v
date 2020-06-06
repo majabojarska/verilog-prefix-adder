6f8e6c8617416c6bd57a944fa3c4244c8fb69978
Maja Bojarska <majabojarska98@gmail.com>
2020-06-06 13:29:14 +0200

    Refactor PG tb

--- qflow/source/pg_tb.v
+++ qflow/source/pg_tb.v
@@ -4,15 +4,15 @@
 module pg_tb;
 
 reg x, y;
-wire gen, prop;
+wire gen, prop, half_sum;
 
 initial begin 
-    $monitor ("{\"x\":\"%b\", \"y\":\"%b\", \"gen\":\"%b\", \"prop\":\"%b\"}", x, y, gen, prop);
+    $monitor ("{\"x\":\"%b\", \"y\":\"%b\", \"gen\":\"%b\", \"prop\":\"%b\", \"half_sum\":\"%b\"}", x, y, gen, prop, half_sum);
     
-    {x, y} = 2'b00;
-    #1 {x, y} = 2'b01;
-    #1 {x, y} = 2'b10;
-    #1 {x, y} = 2'b11;
+    for(int i=0; i<4; i=i+1) begin
+        {x, y} = i;
+        #1;
+    end
     
     #1 $finish;
 end
@@ -22,7 +22,8 @@ pg U0 (
     .x (x),
     .y (y),
     .gen (gen),
-    .prop (prop)
+    .prop (prop),
+    .half_sum (half_sum)
 );
 
 endmodule
